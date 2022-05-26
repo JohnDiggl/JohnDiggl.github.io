a6d39611550f7cf066782e9b1632c036d8d99557
JohnDiggl <simon.tirolf@icloud.com>
2022-05-26 15:04:15 +0200

    first content

---
+++ js/commands.js
@@ -0,0 +1,37 @@
+
+
+help = [
+    "<br>",
+    '<span class="command">whoareyou</span>     Where am I?',
+    '<span class="command">whoareyou</span>     Who are you?',
+    '<span class="command">whoami</span>        Who ami I?',
+    '<span class="command">help</span>          Show all commands',
+    '<span class="command">clear</span>         Clear terminal',
+    '<span class="command">banner</span>        Show the banner again',
+    '<span class="command">history</span>       Show command history',
+    "<br>",
+];
+
+
+whereami = [
+    "<br>",
+    "<span class=\"color2\">You are in my Terminal. Type </span> <span class=\"command\">help</span>.<span class=\"color2\"></span>",
+    "<br>"
+];
+
+whoareyou = [
+    "<br>",
+    "tbc",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "visitor",
+    "<br>"
+];
+
+banner = [
+    '<span class="color2">Welcome.</span>',
+    "<span class=\"color2\">You don't know where you are? Type </span> <span class=\"command\">whereami</span>.<span class=\"color2\"></span>",
+];
